93622f47f8b53e7bd4511740b2766ca266433844
Madictian <fredrikbille@hotmail.com>
2022-03-22 11:50:35 +0100

    make it better

--- routes/defaultRoute.js
+++ routes/defaultRoute.js
@@ -8,7 +8,7 @@ const DataSchema = require('../model/dataScheme')
 router.get('/', async (req, res, next) => {
     try {
       const data = await DataSchema.find({});
-      res.status(200).json({ data });
+      res.status(200).json({ ...data });
     } catch (error) {
       res.status(500).json({
         message: error.message,
